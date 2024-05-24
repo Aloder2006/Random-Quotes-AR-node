335cc21f2ac197488dc8a818f6eb5e0032a62370
Aloder2006 <82025827+Aloder2006@users.noreply.github.com>
2024-05-24 15:58:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,39 +6,41 @@ const path = require('path');
 
 const fs = require('fs');
 
-app.use(cors()); // Prevents CORS error
+app.use(cors());
 
 
 app.get('/api', function(req, res) {
-
-    if (req.url === '/favicon.ico') {
-        res.end();
-    } 
-    // Ends request for favicon without counting
 
     const json = fs.readFileSync('count.json', 'utf-8');
     const obj = JSON.parse(json);
 
     obj.pageviews = obj.pageviews+1;
-    
-    // Updates pageviews and visits (conditional upon URL param value)
+
 
     const newJSON = JSON.stringify(obj);
-    // Converts result to JSON
 
     fs.writeFileSync('count.json', newJSON);
     res.send(newJSON);
-    // Writes result to file and sends to user as JSON
+});
 
-})
+app.get('/visitors', function(req, res){
+    const json = fs.readFileSync('count.json', 'utf-8');
+    const obj = JSON.parse(json);
 
+    res.send(obj);
+});
+    
 
 app.use(express.static(path.join(__dirname, 'vc')));
+
+app.use("/i4rqm", express.static(path.join(__dirname,"instagram.com/i4rqm")));
 
 app.use(function(req, res) {
   res.status(400);
   return res.send(`404 Error: Resource not found`);
 });
+
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
