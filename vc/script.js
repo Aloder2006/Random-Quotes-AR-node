294aac646aed403e91ff10f53e07ba5c537e4781
Aloder2006 <82025827+Aloder2006@users.noreply.github.com>
2024-05-26 23:28:45 +0300

    Update script.js

--- vc/script.js
+++ vc/script.js
@@ -7,54 +7,79 @@ let
   copyBtn = document.querySelector(".copy"),
   vc = document.querySelector(".vc");
   
+let utterance = new SpeechSynthesisUtterance();
 
-
-
+function checkRefresh() {
+  if (document.refreshForm.visited.value == "") {
+    document.refreshForm.visited.value = "1";
+    window.speechSynthesis.cancel(utterance);
+  }
+}
 
 
 function quoteG() {
+
+  if (window.speechSynthesis.speaking) {
+    window.speechSynthesis.cancel(utterance);
+    readBtn.classList.remove("reading");
+    readBtn.innerHTML = `<i class="fa-solid fa-microphone"></i>`;
+  }
+
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "تحميل...";
-  var r = Math.floor(Math.random()* 3777);
+
+
+  var r = Math.floor(Math.random() * 3777);
   fetch(`https://datasets-server.huggingface.co/rows?dataset=HeshamHaroon%2Farabic-quotes&config=default&split=train&offset=${r}&length=1`).then(res => res.json()).then(result => {
     quoteContent.innerText = result.rows[0].row.quote;
     quoteAuthor.innerText = `- ${result.rows[0].row.author}`;
     quoteBtn.classList.remove("loading");
     quoteBtn.innerText = "إقتباس جديد";
-  })
+  });
 }
 
 
 readBtn.addEventListener("click", () => {
-  let utterance = new SpeechSynthesisUtterance();
 
   utterance.rate = .9;
   utterance.lang = 'ar';
   utterance.text = `${quoteContent.innerText}`;
-  readBtn.innerHTML = `<i class="fa-solid fa-microphone-lines"></i>`;
-  readBtn.style.background = "#828282";
-  readBtn.style.color = "#eee";
-  utterance.addEventListener("end", () => {
+
+  if (!window.speechSynthesis.speaking) {
+    readBtn.innerHTML = `<i class="fa-solid fa-spinner"></i>`;
+    readBtn.classList.add("loading");
+
+    utterance.addEventListener("start", () => {
+      readBtn.innerHTML = `<i class="fa-solid fa-microphone-lines"></i>`;
+      readBtn.classList.remove("loading");
+      readBtn.classList.add("reading");
+
+    });
+
+    utterance.addEventListener("end", () => {
+      readBtn.innerHTML = `<i class="fa-solid fa-microphone"></i>`;
+      readBtn.classList.remove("reading");
+    });
+    window.speechSynthesis.speak(utterance);
+  } else {
+    window.speechSynthesis.cancel(utterance);
+    readBtn.classList.remove("reading");
     readBtn.innerHTML = `<i class="fa-solid fa-microphone"></i>`;
-    readBtn.style.background = "#eee";
-    readBtn.style.color = "#828282";
-  });
-  window.speechSynthesis.speak(utterance);
+
+  }
+
 });
 
 
 copyBtn.addEventListener("click", () => {
   navigator.clipboard.writeText(quoteContent.innerText);
   copyBtn.innerHTML = `<i class="fa-solid fa-check"></i>`
-  copyBtn.style.background = "#828282";
-  copyBtn.style.color = "#eee";
+  copyBtn.classList.add("reading");
   setTimeout(() => {
-    copyBtn.style.background = "#eee";
-    copyBtn.style.color = "#828282";
+    copyBtn.classList.remove("reading");
     copyBtn.innerHTML = `<i class="fa-solid fa-copy"></i>`;
   }, 800);
 });
-
 
 twitterBtn.addEventListener("click", () => {
   let urlTwitter = `https://x.com/intent/tweet?url=${quoteContent.innerText} ${quoteAuthor.innerText}`;
@@ -69,5 +94,4 @@ fetch('https://quotes-ar-tdly.onrender.com/api') // Dynamic request with URL par
 .then(res => res.json())
 .then(data => {
   vc.textContent = data.pageviews; 
-  console.log(data);
-})
+});
